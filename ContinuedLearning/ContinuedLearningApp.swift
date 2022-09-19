09e6f0e835cfc8ab2222c3c637b69aa4b1daaa82
Chi Dung <chidung091@gmail.com>
2022-09-19 17:22:38 +0700

    Initial Commit

---
+++ ContinuedLearning/ContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuedLearningApp.swift
+//  ContinuedLearning
+//
+//  Created by Chi Dung on 19/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
